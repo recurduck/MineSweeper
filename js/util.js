8d3c993364db8e9e2654ff771de827925f36aa03
recurduck <81361936+recurduck@users.noreply.github.com>
2021-03-27 02:12:18 +0300

    v0.4

--- js/util.js
+++ js/util.js
@@ -60,6 +60,7 @@ function resetTimer() {
 
 function timerCycle() {
   if (gStopTime == false) {
+      gGame.secsPassed++;
       gSec = parseInt(gSec);
       gMin = parseInt(gMin);
       gSec = gSec + 1;  
@@ -75,6 +76,34 @@ function timerCycle() {
   }
 }
 
+function copyMat(mat) {
+  var board = [];
+  for(var i = 0; i < mat.length; i++) {
+    board[i] = [];
+    for(var j = 0; j < mat[i].length; j++) {
+      board[i][j] = {
+        minesAroundCount: mat[i][j].minesAroundCount,
+        isShown: mat[i][j].isShown,
+        isMine: mat[i][j].isMine,
+        isMarked: mat[i][j].isMarked
+      }
+    }
+  }
+  return board;
+}
+
+function addGameStep() {
+  var board = copyMat(gBoardMatrix);
+  var step = {
+    board,
+    shownCount: gGame.shownCount,
+    markedCount: gGame.markedCount,
+    lives: gGame.lives,
+    hints: gGame.hints,
+    safes: gGame.safes
+  }
+  return step;
+}
 // Dont show the contextmenu od right click
 window.document.oncontextmenu = function(){ 
   return false;
